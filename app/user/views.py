a38080cb2ea28c3c3e11e5516b9702b3ae56fdbf
Vladimir Tambovtsev <tambovcev99@mail.ru>
2020-08-09 18:28:16 +0300

    set up create user api

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from django.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
