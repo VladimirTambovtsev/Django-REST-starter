3cc193e58f95649f13a62cc2b861c051a77b1230
Vladimir Tambovtsev <tambovcev99@mail.ru>
2020-08-11 23:37:53 +0300

    set up listing recipes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
 
 
@@ -13,7 +13,7 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def get_query_sets(self):
+    def get_querysets(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('name')
 
@@ -32,3 +32,15 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in database"""
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """ Manage recipes in the database """
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_querysets(self):
+        """ Retrieve the recipe for the authenticated user """
+        return self.queryset.filter(user=self.request.user)
